e8b5eb5119ef9b6f7780e9a4a0f6dfa51d00e89a
Adam Sheehan <a.t.sheehan@gmail.com>
2023-07-19 19:38:52 -0400

    Create OpenGL context
    To use OpenGL, we need an [OpenGL context][0]. The context is what
    stores the current OpenGL state which we manipulate via
    glFunctions (e.g. storing the background color set via glClearColor).
    
    There are several platform-specific APIs for creating an OpenGL context:
    [WGL][2] for Windows, [CGL][3] for Macs, [GLX][4] for X Windows. There's
    also [EGL][5] which is used by Wayland and mobile clients to create
    OpenGL ES context.
    
    Fortunately, the [glutin][6] library handles cross-platform context
    creation and integrates with winit. There are several steps to the
    process.
    
    The four main concepts we'll encounter are:
    
    * the _display_, which roughly corresponds to some connection to a
    display server,
    * the _surface_, which is something we can draw onto. In our case we are
    using a window surface, but there can also be other surfaces for
    rendering offscreen.
    * the _config_, which describes the surface,
    * and the _context_, which stores the current state of OpenGL.
    
    We also need to [load OpenGL functions][7] which we use the [gl
    crate][8] for.
    
    We then make our first OpenGL call with [gl::ClearColor][9]. This stores
    the color in the active context, so that when we call [gl::Clear][10]
    later it will set the screen to a nice amethyst purple.
    
    [0]: https://www.khronos.org/opengl/wiki/OpenGL_Context
    [1]: https://registry.khronos.org/EGL/specs/eglspec.1.5.pdf
    [2]: https://en.wikipedia.org/wiki/WGL_(API)
    [3]: https://en.wikipedia.org/wiki/Core_OpenGL
    [4]: https://en.wikipedia.org/wiki/GLX
    [5]: https://registry.khronos.org/EGL/specs/eglspec.1.5.pdf
    [6]: https://github.com/rust-windowing/glutin
    [7]: https://www.khronos.org/opengl/wiki/Load_OpenGL_Functions
    [8]: https://crates.io/crates/gl
    [9]:
    https://registry.khronos.org/OpenGL-Refpages/gl4/html/glClearColor.xhtml
    [10]: https://registry.khronos.org/OpenGL-Refpages/gl4/html/glClear.xhtml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::ffi::CString;
+
+use glutin::{
+    config::ConfigTemplateBuilder,
+    context::{ContextApi, ContextAttributesBuilder, Version},
+    display::GetGlDisplay,
+    prelude::{GlDisplay, NotCurrentGlContextSurfaceAccessor},
+    surface::{GlSurface, SurfaceAttributesBuilder},
+};
+use glutin_winit::{DisplayBuilder, GlWindow};
+use raw_window_handle::HasRawWindowHandle;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -6,10 +17,42 @@ use winit::{
 
 fn main() {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("iridium")
-        .build(&event_loop)
+    let window_builder = WindowBuilder::new().with_title("iridium");
+
+    let config_template = ConfigTemplateBuilder::default();
+    let (window, config) = DisplayBuilder::new()
+        .with_window_builder(Some(window_builder))
+        .build(&event_loop, config_template, |mut configs| {
+            configs.next().unwrap()
+        })
         .unwrap();
+    let window = window.unwrap();
+    let display = config.display();
+
+    let context_attributes = ContextAttributesBuilder::new()
+        .with_context_api(ContextApi::OpenGl(Some(Version::new(3, 0))))
+        .build(Some(window.raw_window_handle()));
+
+    let surface_attributes = window.build_surface_attributes(SurfaceAttributesBuilder::default());
+    let surface = unsafe {
+        display
+            .create_window_surface(&config, &surface_attributes)
+            .unwrap()
+    };
+
+    let context = unsafe {
+        display
+            .create_context(&config, &context_attributes)
+            .unwrap()
+            .make_current(&surface)
+            .unwrap()
+    };
+
+    gl::load_with(|s| display.get_proc_address(&CString::new(s).unwrap()));
+
+    unsafe {
+        gl::ClearColor(0.6, 0.4, 0.8, 1.0);
+    }
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
@@ -20,6 +63,13 @@ fn main() {
                 window_id,
             } if window_id == window.id() => {
                 *control_flow = ControlFlow::Exit;
+            }
+            Event::RedrawRequested(window_id) if window_id == window.id() => {
+                unsafe {
+                    gl::Clear(gl::COLOR_BUFFER_BIT);
+                }
+
+                surface.swap_buffers(&context).unwrap();
             }
             _ => (),
         }
