79583357969ce5ffc185eab9154fc903b3b21165
Adam Sheehan <a.t.sheehan@gmail.com>
2023-10-21 19:27:46 -0400

    Extract render module
    There are a lot of details that go into setting up an OpenGL context,
    including several unsafe calls. This change extracts everything related
    to drawing to a new module to keep the main function simpler.
    
    Right now it doesn't handle any errors, we can improve on that later if
    needed.

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,29 @@
-use std::ffi::CString;
+mod render;
 
-use glutin::{
-    config::ConfigTemplateBuilder,
-    context::{ContextApi, ContextAttributesBuilder, Version},
-    display::GetGlDisplay,
-    prelude::{GlDisplay, NotCurrentGlContext},
-    surface::{GlSurface, SurfaceAttributesBuilder},
-};
-use glutin_winit::{DisplayBuilder, GlWindow};
-use raw_window_handle::HasRawWindowHandle;
+use render::Renderer;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoop,
-    window::WindowBuilder,
 };
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let window_builder = WindowBuilder::new().with_title("iridium");
-
-    let config_template = ConfigTemplateBuilder::default();
-    let (window, config) = DisplayBuilder::new()
-        .with_window_builder(Some(window_builder))
-        .build(&event_loop, config_template, |mut configs| {
-            configs.next().unwrap()
-        })
-        .unwrap();
-    let window = window.unwrap();
-    let display = config.display();
-
-    let context_attributes = ContextAttributesBuilder::new()
-        .with_context_api(ContextApi::OpenGl(Some(Version::new(3, 0))))
-        .build(Some(window.raw_window_handle()));
-
-    let surface_attributes = window.build_surface_attributes(SurfaceAttributesBuilder::default());
-    let surface = unsafe {
-        display
-            .create_window_surface(&config, &surface_attributes)
-            .unwrap()
-    };
-
-    let context = unsafe {
-        display
-            .create_context(&config, &context_attributes)
-            .unwrap()
-            .make_current(&surface)
-            .unwrap()
-    };
-
-    gl::load_with(|s| display.get_proc_address(&CString::new(s).unwrap()));
-
-    unsafe {
-        gl::ClearColor(0.6, 0.4, 0.8, 1.0);
-    }
+    let mut renderer = Renderer::new(&event_loop);
 
     event_loop
         .run(move |event, window_target| match event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 window_id,
-            } if window_id == window.id() => {
+            } if window_id == renderer.window_id() => {
                 window_target.exit();
             }
             Event::WindowEvent {
                 event: WindowEvent::RedrawRequested,
                 window_id,
-            } if window_id == window.id() => {
-                unsafe {
-                    gl::Clear(gl::COLOR_BUFFER_BIT);
-                }
-
-                surface.swap_buffers(&context).unwrap();
+            } if window_id == renderer.window_id() => {
+                renderer.clear();
+                renderer.present();
             }
             _ => (),
         })
