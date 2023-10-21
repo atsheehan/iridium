f1d0faab2b6df83ef0601644bc4c4c39264f344d
Adam Sheehan <a.t.sheehan@gmail.com>
2023-10-20 20:11:39 -0400

    Move in direction of heading
    Even though we can look around with the mouse, when we move forward
    we're still traveling along the +Z axis.
    
    Our camera heading is only used in our shader to transform our view of
    the world. We also need to transform our velocity before we update our
    position each tick.
    
    It's possible to rotate a 3D vector by multiplying it with a [rotational
    matrix][0]. This change takes a shortcut by directly applying the matrix
    multiplication to the elements of the vector and returning a new vector
    that's been rotated.
    
    This is also a good time to start adding unit tests to ensure we're
    getting the results we expect. These tests can be run with `cargo test`
    and add some initial converage.
    
    We have to define a custom vec3 assertion because there are often small
    rounding errors after performing multiple operations on floating point
    values. The assert method will ignore these differences when checking if
    two vectors are equal.
    
    [0]: https://gamemath.com/book/matrixtransforms.html#rotation_3d_cardinal_axis

--- src/math.rs
+++ src/math.rs
@@ -15,6 +15,19 @@ impl Vec3 {
     pub(crate) fn set_z(&self, new_z: f32) -> Vec3 {
         Self(self.0, self.1, new_z)
     }
+
+    pub(crate) fn rotate_y(&self, angle: f32) -> Vec3 {
+        let Self(x, y, z) = self;
+
+        let cos = angle.cos();
+        let sin = angle.sin();
+
+        let new_x = x * cos + z * sin;
+        let new_y = *y;
+        let new_z = x * -sin + z * cos;
+
+        Self(new_x, new_y, new_z)
+    }
 }
 
 impl Add<Vec3> for Vec3 {
@@ -44,5 +57,44 @@ impl RandomNumberGenerator {
     pub(crate) fn gen_range(&mut self, min: u32, max: u32) -> u32 {
         let range = max - min;
         min + self.gen_u32() % range
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::f32::consts::*;
+
+    #[test]
+    fn rotate_vector_around_y_axis() {
+        let examples = [
+            (Vec3(1.0, 0.0, 0.0), PI, Vec3(-1.0, 0.0, 0.0)),
+            (Vec3(-1.0, 0.0, 0.0), PI, Vec3(1.0, 0.0, 0.0)),
+            (Vec3(0.5, 0.0, 0.0), FRAC_PI_2, Vec3(0.0, 0.0, -0.5)),
+            (Vec3(0.5, 0.0, 0.0), -FRAC_PI_2, Vec3(0.0, 0.0, 0.5)),
+            (Vec3(0.0, 1.0, 0.0), FRAC_PI_2, Vec3(0.0, 1.0, 0.0)),
+            (
+                Vec3(0.0, 0.0, 1.0),
+                FRAC_PI_4,
+                Vec3(FRAC_1_SQRT_2, 0.0, FRAC_1_SQRT_2),
+            ),
+        ];
+
+        for (input, angle, expected_output) in examples.into_iter() {
+            let actual_output = input.rotate_y(angle);
+            assert_vec3s_equal(&actual_output, &expected_output);
+        }
+    }
+
+    fn assert_vec3s_equal(a: &Vec3, b: &Vec3) {
+        const TOLERANCE: f32 = 0.00001;
+        let vecs_equal = (a.0 - b.0).abs() < TOLERANCE
+            && (a.1 - b.1).abs() < TOLERANCE
+            && (a.2 - b.2).abs() < TOLERANCE;
+        assert!(
+            vecs_equal,
+            "vecs are not equal:\n  left: {:?}\n right: {:?}\n",
+            a, b
+        );
     }
 }
